4b8d0649ef776c341061b9728579eeef53a9d14d
mainak90 <mainak90@gmail.com>
2023-04-16 23:22:32 +0200

    add code comments

--- trickB.go
+++ trickB.go
@@ -4,6 +4,7 @@ import "github.com/iancoleman/strcase"
 
 type TBool int
 
+// Trick Boolean types --> True/False/unSet.
 const (
 	True TBool = 2
 	False TBool = 0
@@ -28,34 +29,49 @@ func min(a, b TBool) TBool {
 	return b
 }
 
+/*
+Implements logical And.
+	False False | False
+	False UnSet | False
+	True  UnSet | UnSet
+ */
 func (a TBool) And(b TBool) TBool {
 	return values[min(a, b)]
 }
 
+/*
+Implements logical Or
+ */
 func (a TBool) Or(b TBool) TBool {
 	return values[max(a, b)]
 }
 
+// Implements logical Nand.
 func (a TBool) Nand(b TBool) TBool {
 	return values[2-min(a, b)]
 }
 
+// Implements logical Not.
 func (a TBool) Not() TBool {
 	return values[2-a]
 }
 
+// Implements logical Nor.
 func (a TBool) Nor(b TBool) TBool {
 	return values[2-max(a, b)]
 }
 
+// Implements logical Xor.
 func (a TBool) Xor(b TBool) TBool {
 	return a.Or(b).And(a.Nand(b))
 }
 
+// Out outputs a TrickBool state as a string representation.
 func (a TBool) Out() string {
 	return strings[a]
 }
 
+// Checks if a Tbool instance isSet or Unset. Returns True if set, else Unset.
 func (a TBool) IsSet() bool {
 	if a == UnSet {
 		return false
@@ -63,6 +79,10 @@ func (a TBool) IsSet() bool {
 	return true
 }
 
+/*
+Outputs a TrickB state type representation from a basic string input.
+Outputs Unset if string does not match bool/unset.
+ */
 func TrickBFromString(s string) TBool {
 	switch strcase.ToCamel(s) {
 	case "UnSet":
